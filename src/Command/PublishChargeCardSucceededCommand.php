0c3fae5433d14041c982b4844963298131d53710
Nicolas Judalet <nicolas.judalet@gmail.com>
2019-07-04 00:16:56 +0200

    Add number of messages option in publisher

--- src/Command/PublishChargeCardSucceededCommand.php
+++ src/Command/PublishChargeCardSucceededCommand.php
@@ -6,6 +6,7 @@ use Swarrot\Broker\Message;
 use Swarrot\SwarrotBundle\Broker\Publisher;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Style\SymfonyStyle;
 
@@ -24,17 +25,22 @@ class PublishChargeCardSucceededCommand extends Command
     {
         $this
             ->setName('publish')
-            ->setDescription('Publishes a new event charge.card.succeeded');
+            ->setDescription('Publishes a new event charge.card.succeeded')
+            ->addOption('numberOfMessages', 'm', InputOption::VALUE_REQUIRED, "Number of messages to publish", 1000);
     }
 
     public function execute(InputInterface $input, OutputInterface $output)
     {
         $io = new SymfonyStyle($input, $output);
 
-        $transactionId = rand(1, 1000);
-        $io->success(sprintf('New card charge succeeded ! Transaction id : %s', $transactionId));
+        $numberOfMessages = $input->getOption('numberOfMessages');
+        $io->success(sprintf('Publishing %s charge.card.succeeded messages', $numberOfMessages));
 
-        $message = new Message(json_encode(['transaction_id' => $transactionId]));
-        $this->publisher->publish('charge_card_succeeded', $message);
+        for ($x = 0; $x < $numberOfMessages; $x++) {
+            $transactionId = rand(1, 1000);
+
+            $message = new Message(json_encode(['transaction_id' => $transactionId]));
+            $this->publisher->publish('charge_card_succeeded', $message);
+        }
     }
 }
