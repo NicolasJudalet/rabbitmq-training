9eec81bfe5b94c3492a4612e9f4e61f5c81eb3c2
Nicolas Judalet <nicolas.judalet@gmail.com>
2019-07-04 08:41:10 +0200

    Add processor throwing error

--- src/Processor/SaveCardProcessorError.php
+++ src/Processor/SaveCardProcessorError.php
@@ -2,7 +2,6 @@
 
 namespace App\Processor;
 
-use Manomano\RabbitmqBundle\Exception\RetryableException;
 use Swarrot\Broker\Message;
 use Swarrot\Processor\ProcessorInterface;
 
@@ -10,6 +9,11 @@ class SaveCardProcessorError implements ProcessorInterface
 {
     public function process(Message $message, array $options)
     {
-        throw new RetryableException('There was an error while saving card');
+        $messageBody = json_decode($message->getBody(), true);
+
+        throw new \Error(sprintf(
+            'The card corresponding to transaction with id %s could not be saved',
+            $messageBody
+        ));
     }
 }
